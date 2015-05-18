aa8122fdd07644a224a4a49cdd9b23b9534b927d
takahiro.iwatani <taka.05022002@gmail.com>
2015-05-18 21:43:43 +0900

    タイトル画面の作成

--- core/src/jp/float1251/miniLD59/screen/TitleScreen.java
+++ core/src/jp/float1251/miniLD59/screen/TitleScreen.java
@@ -1,22 +1,55 @@
 package jp.float1251.miniLD59.screen;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class TitleScreen implements Screen{
+public class TitleScreen implements Screen {
+
+    private final Game game;
+    private SpriteBatch batch;
+    private FitViewport viewport;
+    private Texture title;
+    private BitmapFont font;
+
+    public TitleScreen(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void show() {
-
+        batch = new SpriteBatch();
+        viewport = new FitViewport(240, 160);
+        title = new Texture("title.png");
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        viewport.getCamera().update();
 
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+        batch.draw(title, -title.getWidth() / 2, 10);
+        font.draw(batch, "Tap To Start", -40, -10);
+        batch.end();
+
+        if (Gdx.input.justTouched()) {
+            game.setScreen(new GameScreen());
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
