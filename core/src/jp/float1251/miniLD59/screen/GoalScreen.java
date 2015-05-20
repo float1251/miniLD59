a78ef801041f58ad7d2babd9e1f265f3d9dd2a2d
float1251 <taka.05022002@gmail.com>
2015-05-20 11:53:01 +0900

    ボタンにscaleアクションを追加

--- core/src/jp/float1251/miniLD59/screen/GoalScreen.java
+++ core/src/jp/float1251/miniLD59/screen/GoalScreen.java
@@ -32,24 +32,33 @@ public class GoalScreen implements Screen {
     public void show() {
         stage = new Stage(new FitViewport(210, 160));
 
-        NinePatchDrawable texture = new NinePatchDrawable(new NinePatch(new Texture("button.9.png"), 10, 10, 10, 10));
+        NinePatchDrawable texture = new NinePatchDrawable(new NinePatch(new Texture("button.png"), 10, 10, 10, 10));
         TextButton.TextButtonStyle tbs = new TextButton.TextButtonStyle();
         tbs.font = new BitmapFont();
         tbs.up = texture;
         TextButton textButton = new TextButton("Next", tbs);
+        textButton.setTransform(true);
         textButton.setWidth(80);
-        stage.addActor(textButton);
-
-        textButton = new TextButton("Title", tbs);
-        textButton.setWidth(80);
-        textButton.setPosition(100, 0);
+        textButton.setOrigin(textButton.getWidth() / 2, textButton.getHeight() / 2);
         textButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 Gdx.app.log("test", event.toString());
-                actor.addAction(Actions.sizeTo(actor.getWidth() * 1.1f, actor.getHeight() * 1.1f, 0.5f));
+                actor.addAction(Actions.sequence(Actions.scaleTo(1.1f, 1.1f, 0.1f), Actions.scaleTo(1f, 1f, 0.1f)));
+            }
+        });
+        stage.addActor(textButton);
 
-
+        textButton = new TextButton("Title", tbs);
+        textButton.setTransform(true);
+        textButton.setWidth(80);
+        textButton.setPosition(100, 0);
+        textButton.setOrigin(textButton.getWidth() / 2, textButton.getHeight() / 2);
+        textButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.log("test", event.toString());
+                actor.addAction(Actions.sequence(Actions.scaleTo(1.1f, 1.1f, 0.1f), Actions.scaleTo(1f, 1f, 0.1f)));
             }
         });
         stage.addActor(textButton);
